040a177793829c0d0b8be4ea259a1968f9528144
Namita2290 <83914796+Namita2290@users.noreply.github.com>
2022-11-29 11:13:29 +0530

    Update main.tf

--- Challenge1/Three_tier_arch/main.tf
+++ Challenge1/Three_tier_arch/main.tf
@@ -6,9 +6,9 @@ module "web-tier-module" {
     default_resources_tags = var.default_resources_tags
 }
 
-#invoking business tier module
-module "business-tier-module" {
-    source = "../business-tier"
+#invoking App tier module
+module "app-tier-module" {
+    source = "../app-tier"
     resources_prefix = var.resources_prefix
     location = var.location
     default_resources_tags = var.default_resources_tags
